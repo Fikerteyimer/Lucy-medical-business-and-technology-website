212e19e52f038afedd6b38abe85e7f2cf89c65ad
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-25 16:42:36 +0300

    added footer and news letter component

--- lucy-college/src/Pages/Admission/Calender.jsx
+++ lucy-college/src/Pages/Admission/Calender.jsx
@@ -1,9 +1,14 @@
 import AboutHero from '../../Components/AboutHero';
 import NavBar from '../../Components/NavBar';
+import NewsLetter from '../../Components/NewsLetter';
+import Footer from '../../Components/Footer';
 function Calender(){
     return(
         <div>
             <NavBar/>
+            <NewsLetter/>
+            <Footer/>
+
         </div>
     );
 
