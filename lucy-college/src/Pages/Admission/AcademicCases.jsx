4f6b6e679ebab53395df7634347b20d76c5e0147
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-27 10:09:10 +0300

    Added a hero section to the academic cases

--- lucy-college/src/Pages/Admission/AcademicCases.jsx
+++ lucy-college/src/Pages/Admission/AcademicCases.jsx
@@ -1,10 +1,12 @@
+import AboutHero from "../../Components/AboutHero";
 import Footer from "../../Components/Footer";
 import NavBar from "../../Components/NavBar";
 import NewsLetter from '../../Components/NewsLetter';
 function AcademicCases(){
     return(
         <div>
-            <NavBar/>
+            <NavBar />
+            <AboutHero children="Academic Cases" title="Academic Cases"/>
             <NewsLetter/>
             <Footer/>
            
